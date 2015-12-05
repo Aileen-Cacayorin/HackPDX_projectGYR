52538bf0139f325cff88759cbbb59579dd16cd56
Aileen Cacayorin <acacayorin@gmail.com>
2015-12-05 14:00:47 -0800

    add new student forms

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,22 @@ class StudentsController < ApplicationController
     @teacher = current_teacher
     @students = @teacher.students
   end
+
+  def new
+    @teacher = current_teacher
+    @student = Student.new
+  end
+
+  def create
+    @teacher = current_teacher
+    @student = Student.create(student_params)
+    @student.teacher = @teacher
+    @student.save
+    redirect_to teacher_students_path(@teacher)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :parent_email)
+  end
 end
