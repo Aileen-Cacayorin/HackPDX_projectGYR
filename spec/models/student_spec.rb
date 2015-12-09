75ae426dcfe6f50f2294dd1ccaf7b53960673828
Austin <aokincaid@gmail.com>
2015-12-09 11:44:05 -0800

    Add final unit spec for full_name method on student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Student, type: :model do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :parent_email }
+  it "returns a Student's full name" do
+    student = Student.create(first_name: "Steve", last_name: "Ballmain", parent_email: "bal@main.gov")
+    expect(student.full_name).to eq 'Steve Ballmain'
+  end
 end
