43e0346e9d3c76d7f8b52f42d7f309716857c62d
Austin <aokincaid@gmail.com>
2015-12-10 12:06:38 -0800

    Begin working with daily_summary_params, realizing the models can be slimmed by one

--- app/controllers/daily_summaries_controller.rb
+++ app/controllers/daily_summaries_controller.rb
@@ -12,6 +12,30 @@ class DailySummariesController < ApplicationController
   end
 
   def create
+    binding.pry
+  end
 
+  private
+  def daily_summary_params
+    # date is messed up in params: "date(1i)"=>"2015", "date(2i)"=>"12", "date(3i)"=>"10"
+    params.require(:daily_summary).permit(:date, :text, :homework, daily_report_attributes: [reports_attributes: []])
   end
 end
+
+# What params gives you:
+# {"utf8"=>"✓",
+#  "authenticity_token"=>"tYBAx0zrT1t2VQ9eCtCTTReAvTgp/TdHuiGcIlocBbk+fowVrYEX92TJNINATJ+FQYSAQAyfrjapHA5Qiq6w6g==",
+#  "daily_summary"=>
+#   {"date(1i)"=>"2015",
+#    "date(2i)"=>"12",
+#    "date(3i)"=>"10",
+#    "text"=>"",
+#    "daily_report_attributes"=>
+#     {"reports_attributes"=>
+#       {"0"=>{"color"=>"green", "status"=>"nil", "comment"=>""},
+#        "1"=>{"color"=>"green", "status"=>"nil", "comment"=>""},
+#        "2"=>{"color"=>"green", "status"=>"nil", "comment"=>""},
+#        "3"=>{"color"=>"green", "status"=>"nil", "comment"=>""}}}},
+#  "commit"=>"Save",
+#  "controller"=>"daily_summaries",
+#  "action"=>"create"}
