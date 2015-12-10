e3312609dbdbf8d6e77676a723f23495eb91a371
Austin <aokincaid@gmail.com>
2015-12-10 14:50:32 -0800

    update form to allow model association between class_group and daily_summary

--- app/controllers/daily_summaries_controller.rb
+++ app/controllers/daily_summaries_controller.rb
@@ -11,30 +11,34 @@ class DailySummariesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @class_group = ClassGroup.find(params[:class_group_id])
+    daily_summary = @class_group.daily_summaries.new(daily_summary_params)
+    if daily_summary.save
+      redirect_to root_path
+    end
   end
 
   private
   def daily_summary_params
     # date is messed up in params: "date(1i)"=>"2015", "date(2i)"=>"12", "date(3i)"=>"10"
-    params.require(:daily_summary).permit(:date, :text, :homework, report_attributes: [])
+    params.require(:daily_summary).permit(:date, :text, :homework, reports_attributes: [:student_id, :color, :status, :comment])
   end
 end
 
 # What params gives you:
 # {"utf8"=>"✓",
-#  "authenticity_token"=>"omLoo4uJwfs0nl3FjyASkr63ld4JV+04eBud4A65Md0FmTC89LlplYzTodWpjMdaPMn/kaaym7FF5ClLWNDZEw==",
+#  "authenticity_token"=>"POLVJChv2ofG7al8qhKIhzqw0evTbVTlZkWy2qT0KwWbGQ07V19y6X6gVWyMvl1PuM67pHyIImxbugZx8p3Dyw==",
 #  "daily_summary"=>
 #   {"date(1i)"=>"2015",
 #    "date(2i)"=>"12",
 #    "date(3i)"=>"10",
-#    "text"=>"learning stuff",
-#    "homework"=>"makin' stuff",
+#    "text"=>"leanring",
+#    "homework"=>"homeworkin",
 #    "reports_attributes"=>
-#     {"0"=>{"color"=>"green", "status"=>"absent", "comment"=>"aileen's comment"},
-#      "1"=>{"color"=>"yellow", "status"=>"nil", "comment"=>"Shannon's comment"},
-#      "2"=>{"color"=>"red", "status"=>"nil", "comment"=>"Austin's commment"},
-#      "3"=>{"color"=>"green", "status"=>"nil", "comment"=>"Caterina's comment"}}},
+#     {"0"=>{"student_id"=>"1", "color"=>"green", "status"=>"absent", "comment"=>"aileen"},
+#      "1"=>{"student_id"=>"2", "color"=>"yellow", "status"=>"nil", "comment"=>"Shannon"},
+#      "2"=>{"student_id"=>"3", "color"=>"red", "status"=>"tardy", "comment"=>"Austin"},
+#      "3"=>{"student_id"=>"4", "color"=>"yellow", "status"=>"nil", "comment"=>"caterina"}}},
 #  "commit"=>"Save",
 #  "controller"=>"daily_summaries",
 #  "action"=>"create"}
