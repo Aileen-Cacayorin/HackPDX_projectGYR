3695164739af2f4af02125b439e8d31e16b71485
Austin <aokincaid@gmail.com>
2015-12-10 10:55:16 -0800

    Move form to daily_summary new path as it's more appropriate

---
+++ app/controllers/daily_summaries_controller.rb
@@ -0,0 +1,13 @@
+class DailySummariesController < ApplicationController
+  before_filter :authenticate_teacher!
+
+  def new
+    @teacher = current_teacher
+    @class_group = ClassGroup.find(params[:class_group_id])
+    @daily_summary = @class_group.daily_summaries.new
+    @daily_report = @daily_summary.build_daily_report
+    @class_group.students.each do |student|
+      @daily_report.reports.build(student_id: student.id)
+    end
+  end
+end
